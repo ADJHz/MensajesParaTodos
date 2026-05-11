cd5525fbe9a03a8d2da8c2ef9a8f80a913a822af
Adolfo <adolfodjhz@gmail.com>
2026-05-10 22:37:46 -0600

    feat: instalar y restaurar @cloudflare/vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
-
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
     plugins: [laravel({
